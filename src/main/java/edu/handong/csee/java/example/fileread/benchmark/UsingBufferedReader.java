f7521e38718cacb37ef5322b4075e01dc155b28c
So-Eun Yu <so-eunyu@So-Eunui-MacBookPro.local>
2021-05-20 19:57:18 +0900

    revised code

--- src/main/java/edu/handong/csee/java/example/fileread/benchmark/UsingBufferedReader.java
+++ src/main/java/edu/handong/csee/java/example/fileread/benchmark/UsingBufferedReader.java
@@ -8,12 +8,11 @@ import java.io.*;
 public class UsingBufferedReader {
     public static void main(String[] args) throws IOException {
 
-
         //-------------- Test reading 1 MB file. --------------------
 
         StopWatch.start();
 
-        BufferedReader inputStream= new BufferedReader(new FileReader(DumpDataWriter.input1MB));
+        BufferedReader inputStream= new BufferedReader(new FileReader(args[0]));
         while (inputStream.read()!=-1){}
 
         long duration = StopWatch.stop();
@@ -26,7 +25,7 @@ public class UsingBufferedReader {
 
         StopWatch.start();
 
-        BufferedReader inputStream2= new BufferedReader(new FileReader(DumpDataWriter.input10MB));
+        BufferedReader inputStream2= new BufferedReader(new FileReader(args[1]));
         while (inputStream2.read()!=-1){}
 
         long duration2 = StopWatch.stop();
@@ -39,7 +38,7 @@ public class UsingBufferedReader {
 
         StopWatch.start();
 
-        BufferedReader inputStream3= new BufferedReader(new FileReader(DumpDataWriter.input100MB));
+        BufferedReader inputStream3= new BufferedReader(new FileReader(args[2]));
         while (inputStream3.read()!=-1){}
 
         long duration3 = StopWatch.stop();
@@ -53,7 +52,7 @@ public class UsingBufferedReader {
 
         StopWatch.start();
 
-        BufferedReader inputStream4= new BufferedReader(new FileReader(DumpDataWriter.input1000MB));
+        BufferedReader inputStream4= new BufferedReader(new FileReader(args[3]));
         while (inputStream4.read()!=-1){}
 
         long duration4 = StopWatch.stop();
